dae3427c1a9d6d27e2e2992e5d5418dca6f807c2
Jon Gilkison <jon.gilkison@gmail.com>
2024-05-25 16:12:25 +0700

    Refactor face crop into bounds crop. Added face padding, zoom and gravity.

--- internal/process/images/crop/face.go
+++ internal/process/images/crop/face.go
@@ -3,16 +3,12 @@ package images
 import (
 	"foxy/internal/geometry"
 	"foxy/internal/metadata"
-	"foxy/internal/utils"
-	"github.com/davidbyttow/govips/v2/vips"
-	"log"
 	"math"
+
+	"github.com/davidbyttow/govips/v2/vips"
 )
 
 func cropFace(cW *int, cH *int, imageMeta *metadata.Metadata, params *metadata.ImageParams, sourceImage *vips.ImageRef) (*vips.ImageRef, error) {
-	sw := sourceImage.Width()
-	sh := sourceImage.Height()
-
 	var faceBounds geometry.Box
 	if params.Face.Index != nil && *params.Face.Index < len(imageMeta.Faces) {
 		faceBounds = imageMeta.Faces[*params.Face.Index].Box
@@ -40,71 +36,16 @@ func cropFace(cW *int, cH *int, imageMeta *metadata.Metadata, params *metadata.I
 		}
 	}
 
-	fy := int(math.Floor(faceBounds.Top * float64(sh)))
-	fh := int(math.Floor(faceBounds.Height * float64(sh)))
-
-	var targetWidth int
-	var targetHeight int
-
-	if cW == nil {
-		targetWidth = 10000
-	} else {
-		targetWidth = *cW
-	}
-
-	if cH == nil {
-		targetHeight = 10000
-	} else {
-		targetHeight = *cH
-	}
-
-	cropSize := geometry.SizeToFitSize(targetWidth, targetHeight, sw, sh)
-
-	if params.Zoom != nil {
-		cropSize.Width = int(math.Floor(float64(cropSize.Width) * (1 / *params.Zoom)))
-		cropSize.Height = int(math.Floor(float64(cropSize.Height) * (1 / *params.Zoom)))
-	}
-
-	cx := int((faceBounds.Left + (faceBounds.Width / 2.0)) * float64(sw))
-	var cy int
-	if fh > cropSize.Height {
-		cy = fy + int(math.Floor(float64(fh)/2.0))
-	} else {
-		cy = fy - int(math.Floor((float64(params.Face.Padding)/512.0)*float64(sh)))
-	}
-
-	cropX := utils.Min(sw, utils.Max(0, cx-(cropSize.Width/2)))
-	var cropY int
-	if fh > cropSize.Height {
-		cropY = utils.Min(sw, utils.Max(0, cy-(cropSize.Height/2)))
-	} else {
-		cropY = utils.Min(sh, utils.Max(0, cy))
-	}
-
-	if (cropX + cropSize.Width) > sw {
-		cropX = utils.Max(0, sw-cropSize.Width)
-	}
-
-	if (cropY + cropSize.Height) > sh {
-		cropY = utils.Max(0, sh-cropSize.Height)
-	}
-
-	err := sourceImage.Crop(
-		cropX,
-		cropY,
-		cropSize.Width,
-		cropSize.Height,
+	return cropBounds(
+		cW,
+		cH,
+		faceBounds,
+		params.Zoom,
+		params.Face.Zoom,
+		params.Face.Padding,
+		params.Face.HGravity,
+		params.Face.VGravity,
+		params,
+		sourceImage,
 	)
-	if err != nil {
-		return nil, err
-	}
-
-	if (cropSize.Width != targetWidth) || (cropSize.Height != targetHeight) {
-		err = sourceImage.ThumbnailWithSize(targetWidth, targetHeight, vips.InterestingNone, vips.SizeBoth)
-		if err != nil {
-			log.Fatal("Faces Thumbnail With Size Error:", err)
-		}
-	}
-
-	return sourceImage, nil
 }
